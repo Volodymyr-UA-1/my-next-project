4f52951c92352e99aa61539935ffa01396630f89
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-02-06 00:50:03 +0200

    working with API, useState, Loading, Error

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,29 @@
-export default function NotesPage(){
-  return <h1>Notes</h1>;
-};
+"use client";
+
+import { useState } from "react";
+import NoteList from "@/components/NoteList/NoteList";
+import { getNotes, Note } from "@/lib/api";
+
+const Notes = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleClick = async () => {
+    const response = await getNotes();
+    if (response?.notes) {
+      setNotes(response.notes);
+    }
+  };
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      <button onClick={handleClick}>Get my notes</button>
+      {notes.length > 0 && <NoteList notes={notes} />}
+
+    </section>
+  );
+}
+
+export default Notes;
+
 
